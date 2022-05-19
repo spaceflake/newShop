5f294935d46e6d1cec97984e8750ba928bca3563
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-05-19 14:47:16 +0200

    db connection Fixes #20

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response, NextFunction, Router } from 'express';
 import mongoose from 'mongoose';
+import connectDB from './config/db';
+import { userRouter } from './resources/user/user.router';
 // import bcrypt from 'bcryptjs';
 // import cors from 'cors';
 
@@ -10,17 +12,27 @@ app.use(express.json());
 
 // Routes
 
+app.use('/api', userRouter);
+
 // 404 handler
 
 // global error handler
 
-mongoose.connect('mongodb://localhost:27017/hatsonhats', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('Connection to database established!');
-    app.listen(port, () => {
-      console.log(`server is running on ${port}`);
-    });
-  }
+// mongoose.connect(
+//   'mongodb+srv://hatmaker:hatpassword101@hatsonhatsdb.dba57.mongodb.net/?retryWrites=true&w=majority',
+//   (err) => {
+//     if (err) {
+//       console.error(err);
+//     } else {
+//       console.log('Connection to database established!');
+//       app.listen(port, () => {
+//         console.log(`server is running on ${port}`);
+//       });
+//     }
+//   }
+// );
+
+connectDB();
+app.listen(port, () => {
+  console.log(`server is running on ${port}`);
 });
