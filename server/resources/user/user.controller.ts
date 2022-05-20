5704d7d4a39a3aee2753a481537b8c376bd9b89f
Hazem Kawas <hazem.k.4488@gmail.com>
2022-05-20 09:23:04 +0200

    add log in with passport

--- server/resources/user/user.controller.ts
+++ server/resources/user/user.controller.ts
@@ -2,6 +2,68 @@ import { NextFunction, Request, Response } from 'express';
 import { UserModel, DbUserInterface, UserInterface } from './user.model';
 import bcrypt from 'bcrypt';
 
+import passport from 'passport';
+import passportLocal from 'passport-local';
+
+const myStrategy = passportLocal.Strategy
+
+  passport.use(new myStrategy((email: string, password: string, done) => {
+    UserModel.findOne({ email: email }, (err: any, user: DbUserInterface) => {
+      if (err) throw err;
+      if (!user) return done(null, false, { message: 'No user found' });
+      bcrypt.compare(password, user.password, (err, result: boolean) => {
+        if (err) throw err;
+        if (result === true) {
+          return done(null, user);
+        } else {
+          return done(null, false, { message: 'Password incorrect' });
+        }
+      });
+    });
+  })
+  );
+  
+  passport.serializeUser((user: any, cb: any) => {
+    cb(null, user._id);
+  });
+  passport.deserializeUser((id: string,  cb) => {
+  UserModel.findOne({ _id: id }, (err: any, user: DbUserInterface) => {
+  const userInformation: UserInterface = {
+    firstName: user?.firstName,
+    lastName: user?.lastName,
+    email: user?.email,
+    isAdmin: user?.isAdmin,
+    id: user?._id
+  };
+  cb(err, userInformation);
+  });
+  });
+// Login Routes
+export const handleLogin = (req: Request, res: Response) => {
+  passport.authenticate("local"), (req :Request, res:Response) => {
+    res.send("success")
+}
+}
+
+// app.post("/api/login", passport.authenticate("local"), (req :Request, res:Response) => {
+//   res.send("success")
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- server/server.ts
+++ server/server.ts
@@ -3,14 +3,30 @@ import mongoose from 'mongoose';
 import connectDB from './config/db';
 import errorHandler from './middleware/errorMiddleware';
 import { userRouter } from './resources/user/user.router';
+import { UserModel, DbUserInterface, UserInterface } from './resources/user/user.model';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
+//passport
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import bcrypt from 'bcrypt';
 
-// import bcrypt from 'bcryptjs';
-// import cors from 'cors';
 
 const app = express();
 const port = 4000;
 // Global middlewares
 app.use(express.json());
+
+app.use(
+  session({
+    secret: "secretCode",
+    resave: false,
+    saveUninitialized: false,
+  })
+  );
+  app.use(cookieParser());
+  app.use(passport.initialize());
+  app.use(passport.session());
 
 // Routes
 
@@ -20,6 +36,11 @@ app.use('/api', userRouter);
 
 // global error handler
 app.use(errorHandler);
+
+
+
+
+
 
 connectDB();
 app.listen(port, () => {
