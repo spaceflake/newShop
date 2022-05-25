91e4f5d1c1f07d99e089c68341017317f2f1505f
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-05-25 11:59:01 +0200

    Prepared fom logo img

--- server/resources/delivery/delivery.controller.ts
+++ server/resources/delivery/delivery.controller.ts
@@ -6,7 +6,7 @@ export const getAllDeliverys = async (req: Request, res: Response) => {
   res.status(200).json(deliverys);
 };
 export const getDelivery = async (req: Request, res: Response) => {
-  const { id } = req.body;
+  const { id } = req.params;
   const delivery = await DeliveryModel.findById(id);
   res.status(200).json(delivery);
 };
@@ -28,6 +28,15 @@ export const updateDelivery = async (req: Request, res: Response) => {
   res.status(200).json(deliverys);
 };
 export const deleteDelivery = async (req: Request, res: Response) => {
-  const deliverys = await DeliveryModel.find({});
-  res.status(200).json(deliverys);
+  const { id } = req.params;
+  if (id || req.body.isAdmin) {
+    try {
+      const deliveryOption = await DeliveryModel.findByIdAndDelete(id);
+      return res.status(200).json('DELETED DELIVERY OPTION');
+    } catch (err) {
+      return res.status(500).json('error');
+    }
+  } else {
+    return res.status(403).json('can not delete ');
+  }
 };
--- server/resources/delivery/delivery.model.ts
+++ server/resources/delivery/delivery.model.ts
@@ -1,24 +1,32 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema, Types } from 'mongoose';
 
 export interface Delivery {
   name: string;
   altText: string;
   shippingTime: number;
   price: number;
-  logo: string;
+  logoId: Types.ObjectId;
+  logoUrl: string;
   id: string;
 }
 
-const deliverySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  altText: { type: String, required: true },
-  shippingTime: { type: Number, required: true },
-  price: { type: Number, required: true },
-  logo: { type: String, required: true },
-  id: { type: String, required: true },
+const deliverySchema = new mongoose.Schema<Delivery>(
+  {
+    name: { type: String, required: true },
+    altText: { type: String, required: true },
+    shippingTime: { type: Number, required: true },
+    price: { type: Number, required: true },
+    logoId: { type: Schema.Types.ObjectId, required: true },
+    id: { type: String, required: true },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+deliverySchema.virtual('logoUrl').get(function () {
+  return '/api/media/' + this.logoUrl;
 });
 
-export const DeliveryModel = mongoose.model<Delivery>(
-  'delivery',
-  deliverySchema
-);
+export const DeliveryModel = mongoose.model('delivery', deliverySchema);
