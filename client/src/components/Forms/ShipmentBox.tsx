c6e846f40c479a4337cc6f8d5d43d1123c095c04
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-05-25 10:54:07 +0200

    get deliveryOptions from db to client

--- client/src/components/Forms/ShipmentBox.tsx
+++ client/src/components/Forms/ShipmentBox.tsx
@@ -6,23 +6,36 @@ import {
   ListItemAvatar,
   Avatar,
   Typography,
-} from "@mui/material";
-import addDays from "date-fns/addDays";
-import format from "date-fns/format";
-import sv from "date-fns/locale/sv";
-import { FormikProps } from "formik";
-import React from "react";
-import { Delivery, deliveryOptions } from "../../Api/Data";
-import { OrderData } from "./OrderForm";
+} from '@mui/material';
+import axios from 'axios';
+import addDays from 'date-fns/addDays';
+import format from 'date-fns/format';
+import sv from 'date-fns/locale/sv';
+import { FormikProps } from 'formik';
+import React, { useEffect, useState } from 'react';
+import { Delivery, deliveryOptions } from '../../Api/Data';
+import { OrderData } from './OrderForm';
 
 interface Props {
   formikProps: FormikProps<OrderData>;
-  setShippingMethod: React.Dispatch<React.SetStateAction<number | undefined>>
+  setShippingMethod: React.Dispatch<React.SetStateAction<number | undefined>>;
 }
 
 function ShipmentBox(props: Props) {
   // the state to  handle clicks
-  const [selectedIndex, setSelectedIndex] = React.useState<number | undefined>(undefined);
+  const [selectedIndex, setSelectedIndex] = React.useState<number | undefined>(
+    undefined
+  );
+  const [deliveryOptions, setDeliveryoptions] = useState<Delivery[]>([]);
+
+  useEffect(() => {
+    const getDeliveryOptions = async () => {
+      const res = await axios.get('http://localhost:4000/api/delivery');
+      const data = await res.data;
+      setDeliveryoptions(data);
+    };
+    getDeliveryOptions();
+  }, []);
 
   // Checks which button is clicked
   const handleListItemClick = (
@@ -32,20 +45,20 @@ function ShipmentBox(props: Props) {
     // if clicked, sets value (seen in orderDetails) to chosen method
     {
       setSelectedIndex(index);
-      props.setShippingMethod(index)
-      props.formikProps.setFieldValue("shippingMethod", index);
+      props.setShippingMethod(index);
+      props.formikProps.setFieldValue('shippingMethod', index);
     };
 
   return (
     // The full "form" for delivery
-    <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+    <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
       <List component="nav" aria-label="Shipping options list">
         {/* loops through array of delivery options */}
         {deliveryOptions.length !== 0 &&
           deliveryOptions.map((delivery: Delivery, index) => (
             <React.Fragment key={delivery.id}>
               {/* displays all objects in array based on index */}
-              <ListItemButton 
+              <ListItemButton
                 selected={selectedIndex === index}
                 onClick={(
                   event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -63,28 +76,28 @@ function ShipmentBox(props: Props) {
                     <>
                       {/* altText for delivery-option */}
                       <Typography
-                        sx={{ display: "block" }}
+                        sx={{ display: 'block' }}
                         component="span"
                         variant="body2"
                       >{`${delivery.altText}`}</Typography>
 
                       {/* Price for delivery-option  */}
                       <Typography
-                        sx={{ display: "block" }}
+                        sx={{ display: 'block' }}
                         component="span"
                         variant="body2"
                       >{`Leveranskostnad: ${delivery.price} kr`}</Typography>
 
                       {/* delivery time */}
                       <Typography
-                        sx={{ display: "block" }}
+                        sx={{ display: 'block' }}
                         component="span"
                         variant="body2"
 
                         // calculates the expected delivery date based on today's date + shippingTime
                       >{`Senaste datum för leverans: ${format(
                         addDays(new Date(), delivery.shippingTime),
-                        "d MMMM",
+                        'd MMMM',
                         { locale: sv }
                       )}`}</Typography>
                     </>
