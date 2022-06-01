6bdb6c7125c45070894b1f863eba704a183d4acb
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-06-01 10:59:54 +0200

    start

--- client/src/pages/Admin/AdminPage.tsx
+++ client/src/pages/Admin/AdminPage.tsx
@@ -1,54 +1,46 @@
-import AdminProductControl from "./AdminProductControl";
-import React, { useState } from 'react'
-import { Box, Button,  } from '@mui/material'
-import Container from '@mui/material/Container/Container'
-import AdminUserControl from "./AdminUserControl";
-import { Link } from "react-router-dom";
+import AdminProductControl from './AdminProductControl';
+import React, { useState } from 'react';
+import { Box, Button } from '@mui/material';
+import Container from '@mui/material/Container/Container';
+import AdminUserControl from './AdminUserControl';
+import { Link } from 'react-router-dom';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
-
 function AdminPage() {
-  const [activePage, setActivePage] = useState('products')
-  return (
-    <><Container maxWidth="lg">
-        <Link to="/">
-      <Button startIcon={<ArrowBackIcon />}>Back to home page</Button>
-    </Link>
-      <Box
-        sx={{
-          display: 'flex',
-          margin: '1rem',
-          gap: '1rem'
-        }}
-      >
+  const [activePage, setActivePage] = useState('products');
 
-   
-          <Button
-            variant="contained"
-            onClick={() => setActivePage('users')}
-          >
+  // add order resource
+
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Link to="/">
+          <Button startIcon={<ArrowBackIcon />}>Back to home page</Button>
+        </Link>
+        <Box
+          sx={{
+            display: 'flex',
+            margin: '1rem',
+            gap: '1rem',
+          }}
+        >
+          <Button variant="contained" onClick={() => setActivePage('users')}>
             Users
           </Button>
 
-          <Button
-            variant="contained"
-            onClick={() => setActivePage('products')}
-          >
+          <Button variant="contained" onClick={() => setActivePage('products')}>
             Products
           </Button>
-  
-          <Button
-            variant="contained"
-            onClick={() => setActivePage('orders')}
-          >
+
+          <Button variant="contained" onClick={() => setActivePage('orders')}>
             Orders
           </Button>
-      </Box>
-    </Container>
-      {activePage === "users" && <AdminUserControl />}
-      {activePage === "products" && <AdminProductControl />}
+        </Box>
+      </Container>
+      {activePage === 'users' && <AdminUserControl />}
+      {activePage === 'products' && <AdminProductControl />}
     </>
-    )
+  );
 }
- 
+
 export default AdminPage;
