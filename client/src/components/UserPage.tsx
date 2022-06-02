d655b9c61dd97b6d1333df1e0e319d6cbc85824e
Hazem Kawas <hazem.k.4488@gmail.com>
2022-06-02 13:01:27 +0200

    display user order with the statuse

--- client/src/components/UserPage.tsx
+++ client/src/components/UserPage.tsx
@@ -1,26 +1,43 @@
-import { Box, Button, Checkbox, FormControlLabel, Typography } from "@mui/material";
+import { Box, Button, Checkbox, FormControlLabel, Grid, List, Typography } from "@mui/material";
 import { FormEvent, useEffect, useState } from "react";
 import { useUser } from '../contexts/UserContext';
 import axios, { AxiosResponse } from 'axios';
-import { Order } from '@shared/types';
+import { Order, Product } from '@shared/types';
 
   
 export default function UserPage() {
     const { user } = useUser();
     const [userOrders, setUserOrders] = useState<Order[]>([]);
+    const [orderPrdocuts, setOrderPrdocuts] = useState<any[]>([]);
+    const [userOrdersdeliveryAddress, setUserOrdersdeliveryAddress] = useState<any[]>([]);
     console.log(user?.id);
-    
+
+
+    // const orserProducts = userOrders.filter()
+    const getOrderDetails = () => {
+        for (let i = 0; i < userOrders.length; i++) {
+            const order = userOrders[i];
+            setOrderPrdocuts(order.products)
+            setUserOrdersdeliveryAddress(order.deliveryAddress)
+            console.log('deliveryAddress', order.deliveryAddress);
+            console.log("products", order.products)
+        }
+    }
+   
+
+
+    // const OrderAddress = userOrders.filter()
+
+
     useEffect(() => {
         const getUserOrders = async () => {
           const res = await axios.get('/api/users-orders/' + user?.id);
           const userOrders = await res.data;
-    
           setUserOrders(userOrders);
-          console.log(userOrders);
-          
+          console.log('userOrders', userOrders);
         };
-    
         getUserOrders();
+        getOrderDetails()
       }, [user?.id]);
     function handleSubmit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault()
@@ -31,7 +48,24 @@ export default function UserPage() {
         <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
             <Typography variant="h6">Your previous orders:</Typography>
             <Box>
-
+            {userOrders.map((order) => (
+            <Grid key={order.id} item xs={12} sm={6} md={4} lg={3}>
+             Your order with the num: {order.id} 
+              {order.isSent? <p> has been sent</p> : <p>not sent yet</p>}
+              {/* {orderPrdocuts.map((product) => (
+            <List key={product.id} >
+              {product.title}
+            </List>
+          ))}
+              {userOrdersdeliveryAddress.map((Address, n) => (
+            <List key={n} >
+              {Address.city}
+            </List>
+          ))} */}
+            </Grid>
+          ))}
+           
+          
             </Box>
 
             <form style={{ marginTop: '2rem', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }} onSubmit={(e) => handleSubmit(e)}>
