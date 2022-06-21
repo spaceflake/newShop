2f18671ba656f8cec3a5edc8a3c8e18d8482360b
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-06-21 09:11:11 +0200

    fixed alignment

--- client/src/components/StartPageDescription.tsx
+++ client/src/components/StartPageDescription.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import { Box, Button, Typography } from '@mui/material';
-import hatLogo from '../assets/hatLogo.png';
 import PopularDucks from './Cards/PopularDucks';
 import ShowCarousel from './Carousel';
 
@@ -8,33 +7,26 @@ function StartPageDesription() {
   return (
     <Box
       sx={{
-        width: "100%",
-        height: "fitContent",
-        background: "#ffffff",
-        paddingTop: "2rem",
+        width: '90%',
+        height: 'fitContent',
+        background: '#ffffff',
+        marginInline: 'auto',
       }}
     >
       <ShowCarousel />
       <Box
         sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          flexDirection: "column",
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
         }}
       >
-        <img
-          style={{
-            width: "10rem",
-          }}
-          src={hatLogo}
-          alt="logo"
-        ></img>
         <Typography
           sx={{
-            textAlign: "center",
-            paddingTop: "1rem",
-            fontSize: "clamp(1.5rem, 2.5vw, 2rem)",
+            textAlign: 'center',
+            paddingTop: '1rem',
+            fontSize: 'clamp(1.5rem, 2.5vw, 2rem)',
           }}
         >
           Welcome to Hats on Hats, we got the hats you need!
@@ -42,22 +34,25 @@ function StartPageDesription() {
       </Box>
       <Box
         sx={{
-          width: "70%",
-          textAlign: "center",
-          margin: "auto",
-          paddingTop: "1rem",
-          paddingBottom: "2rem",
+          width: '70%',
+          textAlign: 'center',
+          margin: 'auto',
+          paddingTop: '1rem',
+          paddingBottom: '2rem',
         }}
       >
-        <Typography sx={{ fontSize: "clamp(1rem, 2.5vw, 1.1rem)" }}>
+        <Typography sx={{ fontSize: 'clamp(1rem, 2.5vw, 1.1rem)' }}>
           Here at Hats on Hats we got hats for everyone!
           {<br />}
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt accusamus quod neque. Fugit qui, maiores nostrum ratione ipsam quidem obcaecati culpa itaque iusto, ut numquam incidunt, ex aperiam. Beatae, ullam.
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nesciunt
+          accusamus quod neque. Fugit qui, maiores nostrum ratione ipsam quidem
+          obcaecati culpa itaque iusto, ut numquam incidunt, ex aperiam. Beatae,
+          ullam.
         </Typography>
       </Box>
-      <Box sx={{ textAlign: 'center' }}>
+      <Box sx={{ textAlign: 'left', marginTop: '5vw' }}>
         <Typography
-          sx={{ marginBottom: "2rem", fontSize: "clamp(1.5rem, 2.5vw, 2rem)" }}
+          sx={{ marginBottom: '2rem', fontSize: 'clamp(1.5rem, 2.5vw, 2rem)' }}
         >
           Our popular products
         </Typography>
@@ -67,11 +62,11 @@ function StartPageDesription() {
             sx={{
               mt: 2,
               mb: 2,
-              height: "3rem",
-              bgcolor: "#ED6C02",
-              border: "none",
-              color: " white",
-              "&:hover": {
+              height: '3rem',
+              bgcolor: '#ED6C02',
+              border: 'none',
+              color: ' white',
+              '&:hover': {
                 bgcolor: '#181818',
                 color: 'white',
               },
