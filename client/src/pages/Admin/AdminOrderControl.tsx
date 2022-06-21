11f5469fc6069680b5fcdb11918afb3e1851bffe
Tomas Fridekrans <51060774+spaceflake@users.noreply.github.com>
2022-06-21 10:41:56 +0200

    added table

--- client/src/pages/Admin/AdminOrderControl.tsx
+++ client/src/pages/Admin/AdminOrderControl.tsx
@@ -1,7 +1,126 @@
-import { Button, Checkbox, List, ListItem, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  Checkbox,
+  Collapse,
+  Container,
+  IconButton,
+  List,
+  ListItem,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from '@mui/material';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import { Order } from '@shared/types';
 import axios, { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
+
+interface OrderProp {
+  order: Order;
+}
+
+const OrderRow = ({ order }: OrderProp) => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
+        <TableCell>
+          <IconButton
+            aria-label="expand row"
+            size="small"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {order.id}
+        </TableCell>
+        <TableCell align="right">{order.user}</TableCell>
+        <TableCell align="right">{order.createdAt}</TableCell>
+        <TableCell align="right">{order.shippingMethod}</TableCell>
+        <TableCell align="right">
+          {order.isSent ? 'shipped' : 'not shipped'}
+        </TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box sx={{ margin: 1 }}>
+              <Typography variant="h6" gutterBottom component="div">
+                Order details
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Product name</TableCell>
+                    <TableCell align="right">Amount</TableCell>
+                    <TableCell align="right">Total price (SEK)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {order.products.map((productRow) => (
+                    <TableRow key={productRow.title}>
+                      <TableCell component="th" scope="row">
+                        {productRow.title}
+                      </TableCell>
+                      <TableCell align="right">{productRow.qty}</TableCell>
+                      <TableCell align="right">
+                        {Math.round(productRow.qty * productRow.price * 100) /
+                          100}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <Typography variant="h6" gutterBottom component="div">
+                Delivery details
+              </Typography>
+              <Table size="small" aria-label="delivery">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Firstname</TableCell>
+                    <TableCell align="right">Lastname</TableCell>
+                    <TableCell align="right">Street</TableCell>
+                    <TableCell align="right">City</TableCell>
+                    <TableCell align="right">Zipcode</TableCell>
+                    <TableCell align="right">Email</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {order.deliveryAddress.map((deliveryRow) => (
+                    <TableRow key={deliveryRow.email}>
+                      <TableCell component="th" scope="row">
+                        {deliveryRow.firstName}
+                      </TableCell>
+                      <TableCell align="right">
+                        {deliveryRow.lastName}
+                      </TableCell>
+                      <TableCell align="right">{deliveryRow.street}</TableCell>
+                      <TableCell align="right">{deliveryRow.city}</TableCell>
+                      <TableCell align="right">{deliveryRow.zipcode}</TableCell>
+                      <TableCell align="right">{deliveryRow.email}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+              <Button>Mark as sent</Button>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </>
+  );
+};
+
 const AdminOrderControl = () => {
   // state for checkbox
   const [orders, setOrders] = useState<Order[]>([]);
@@ -38,30 +157,51 @@ const AdminOrderControl = () => {
   };
 
   return (
-    <>
-      {orders.map((order) => (
-        <List key={order.id}>
-          <ListItem sx={{ marginBlock: '1rem' }}>
-            <Typography variant="body1">{order.id}</Typography>
-            {order.isSent === false ? (
-              <>
-                <Checkbox
-                  onChange={() => {
-                    setSelectedorder(order.id);
-                  }}
-                />
-                <Button
-                  onClick={handleClick}
-                  disabled={selectedorder === order.id ? false : true}
-                >
-                  Mark as sent
-                </Button>
-              </>
-            ) : null}
-          </ListItem>
-        </List>
-      ))}
-    </>
+    <Container>
+      <TableContainer component={Paper}>
+        <Table aria-label="collapsible table">
+          <TableHead sx={{ backgroundColor: 'hsl(214, 100%, 76%)' }}>
+            <TableRow>
+              <TableCell />
+              <TableCell>Order Id</TableCell>
+              <TableCell align="right">Customer</TableCell>
+              <TableCell align="right">Order date</TableCell>
+              <TableCell align="right">Delivery Company</TableCell>
+              <TableCell align="right">Status</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {orders.map((order) => (
+              <OrderRow key={order.id} order={order} />
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+    // <>
+    //   {orders.map((order) => (
+    //     <List key={order.id}>
+    //       <ListItem sx={{ marginBlock: '1rem' }}>
+    //         <Typography variant="body1">{order.id}</Typography>
+    //         {order.isSent === false ? (
+    //           <>
+    //             <Checkbox
+    //               onChange={() => {
+    //                 setSelectedorder(order.id);
+    //               }}
+    //             />
+    //             <Button
+    //               onClick={handleClick}
+    //               disabled={selectedorder === order.id ? false : true}
+    //             >
+    //               Mark as sent
+    //             </Button>
+    //           </>
+    //         ) : null}
+    //       </ListItem>
+    //     </List>
+    //   ))}
+    // </>
   );
 };
 
